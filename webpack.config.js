d09b9882eca9c98259380f6f823da850c7379cc1
Joshua Johanan <josh359@gmail.com>
2016-12-28 12:50:37 -0500

    Added Babel so the source could be uglified.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
+const path = require('path');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: './static',
     filename: 'app.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        include: [path.resolve(__dirname, 'src')],
+        test: /\.js$/,
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
   }
 }
